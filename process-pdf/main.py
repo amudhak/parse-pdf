1d30ddbdce52d77c5126623c9307b8e338314d80
Amudha <amudha@Amudhas-MacBook-Pro.local>
2024-07-23 12:42:26 -0400

    process multiple files

--- process-pdf/main.py
+++ process-pdf/main.py
@@ -1,10 +1,8 @@
 import re   # Regular Expression module
+import os
 import json
 from app import aiPost
 from pdfminer.high_level import extract_text
-from langchain_community.llms import Ollama
-
-llm = Ollama(model="llama3")
     
 def parse_pdf(file):
     text = extract_text(file)
@@ -16,9 +14,11 @@ def process_query(query, context):
     return response
 
 def process_text(text):
-    summary = process_query("Write a summary from the given text: ", text)
-    key_points = process_query("Write out some key points from the given text: ", text)
+    title = process_query("Simply return the given title of the article: ", text)
+    summary = process_query("Write a summary from the given text without a heading: ", text)
+    key_points = process_query("Simply return some key points from the given text as a non-bulleted list (example: [x,y,z]): ", text)
     return {
+        "title": title,
         "summary": summary,
         "key_points": [
             key_points
@@ -29,22 +29,28 @@ def write_json_to_file(data, file_path):
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=4)
 
-def main(pdf_path, json_file_path):
+def main(pdf_path, json_file_path, index):
     extracted_text = parse_pdf(pdf_path)
     context_text = " ".join(extracted_text)     # Convert to string
     processed_data = process_text(context_text)
-    
+    num = str(index)
     json_data = {
-        "article_id": "1",
-        "title": "Article",
+        "article_id": num,
+        "title": processed_data["title"],
         "summary": processed_data["summary"],
         "key_points": processed_data["key_points"]
     }
-    
     write_json_to_file(json_data, json_file_path)
 
 # Run Program
 if __name__ == "__main__":
-    pdf_path = "PDF_Files/file.pdf" # Update pdf file link here
-    json_file_path = "Output.json"
-    main(pdf_path, json_file_path)
+    directory = "PDF_Files"
+    index = 1
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        if os.path.isfile(f):   # Check if is file
+            pdf_path = f"PDF_Files/file{index}.pdf"
+            # pdf_path = "PDF_Files/file.pdf" # Update pdf file link here
+            json_file_path = "Output.json"
+            main(pdf_path, json_file_path, index)
+            index += 1
